376d4fb06fbda21c936a04ffb66fedbd50dfdcce
Ali Jomeiy <ali.jomeiy@gmail.com>
2025-05-08 20:27:31 +0330

    some typo stuff

--- server/session/admin.py
+++ server/session/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Session
 
+
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('start_time', 'end_time', 'start_register_time', 'end_register_time', 'status')
