c49ec4f26b6a29cbc917b1fab1202a16e12394cd
Ali Jomeiy <ali.jomeiy@gmail.com>
2025-05-09 20:42:44 +0330

    Fix validation

--- server/user/models.py
+++ server/user/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -12,8 +13,12 @@ class User(models.Model):
     NUMBER_CHOICES = [(i, str(i)) for i in range(1, 121)]
 
     name = models.CharField(max_length=100)
-    created_by = models.ForeignKey(MessengerUser, on_delete=models.CASCADE, related_name='created_users')
-    default_hezb = models.IntegerField(choices=NUMBER_CHOICES)
+    created_by = models.ForeignKey(MessengerUser, on_delete=models.CASCADE,
+                                   related_name='created_users')
+    default_hezb = models.IntegerField(null=True, blank=True, default=None,
+                                       validators=[MinValueValidator(1),
+                                                   MaxValueValidator(120)])
 
     def __str__(self):
-        return f"User: {self.name} Created by: {self.created_by} ==> default hezb: ({self.default_hezb})"
+        return (f"User: {self.name} Created by: {self.created_by} ==> default "
+                f"hezb: ({self.default_hezb})")
