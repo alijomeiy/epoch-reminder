116fe88cb4978e6f856e0be61483cb0705422ff7
soroush.m <sorooshm78@gmail.com>
2025-05-11 10:11:21 +0000

    dockerize

--- server/server/settings.py
+++ server/server/settings.py
@@ -30,8 +30,8 @@ ALLOWED_HOSTS = [
 ]
 
 # Celery config
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 # Application definition
